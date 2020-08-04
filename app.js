7f0de3c9b318bf9e69ca0b5e78331e2284727480
puvvadaharsha <puvvadaharsha@gmail.com>
2020-08-04 14:08:42 -0500

    Added random box function.

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
+// List to store boxes
+let boxes = [];
+
 document.addEventListener('DOMContentLoaded', ()=>{
-    const grid = document.querySelector(".grid");
-    
+    initializeGrid();
+    addRandom();
 })
+
+// initializes Grid with '0' boxes
+function initializeGrid(){
+    const grid = document.querySelector(".grid");
+    for(let i = 0; i < 16; i++){
+
+        // create new div with class of box and value of 0
+        let box = document.createElement('div');
+        box.classList.add('box');
+        box.textContent = '0';
+
+        // store the box in list
+        boxes.push(box);
+
+        // add the box to the grid
+        grid.appendChild(box);
+
+    }
+}
+
+// adds random box of value 2
+function addRandom(){
+    // pick random number between 0 and 15
+    let random = Math.floor(Math.random() * 16);
+
+    //Add 2 only if box value is 0
+    if(boxes[random].textContent === "0"){
+        boxes[random].textContent = "2";
+    } else{addRandom();}
+}
